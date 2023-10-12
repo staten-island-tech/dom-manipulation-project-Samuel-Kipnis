512b10c1c18c911ebd7509e487f6fb97532028b6
Samiksusamik <sam.kipnis@gmail.com>
2023-10-12 10:14:13 -0400

    e

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-const DOMSelectors = {
-    username: '#username', email: '#email', password: '#password'
-}
-const DOMObjects = {}
-for(let selector in Object.keys) {
-    const DOMObject = document.querySelector(DOMSelectors[selector])
-    DOMObjects[selector] = DOMObject
+const DOMSelectors = ['username', 'email', 'password']
+
+function getDOMObject(selector) {
+    return document.getElementById(DOMSelectors[selector])
 }
 
-console.log(DOMObjects)
+function insertData(htmlElement, data) {
+    htmlElement.innerText = data
+}
+
+const outputDisplay = document.querySelector('#outputDisplay')
+
+document.querySelector('#form').addEventListener('submit', (e) => {
+    let output = ''
+    for(let selector in DOMSelectors) {
+        const input = getDOMObject(selector)
+        const value = input.value
+        output += `${DOMSelectors[selector].charAt(0).toUpperCase() + DOMSelectors[selector].slice(1)}: ${input.value}\n`
+    }
+    console.log(output);
+    insertData(outputDisplay, output)
+})
