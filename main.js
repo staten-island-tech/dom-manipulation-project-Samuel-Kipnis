18948feae2aa31b348f1d226aa8d42bb105488b6
Samiksusamik <sam.kipnis@gmail.com>
2023-10-20 09:40:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const DOMSelectors = ['username', 'email', 'password']
+let elements = ''
 
 function getDOMObject(selector) {
     return document.getElementById(DOMSelectors[selector])
 }
 
 function insertData(htmlElement, data) {
-    let elements = ''
     data.forEach(el => {
         elements += `<p class='p-2 my-3 shadow-md border rounded'>${el}</p>`
     });
+    elements += '<br>'
     htmlElement.innerHTML = elements
 }
 
@@ -19,6 +20,7 @@ function clearInput(selector) {
 const outputDisplay = document.querySelector('#outputDisplay')
 
 document.querySelector('#form').addEventListener('submit', (e) => {
+    e.preventDefault()
     const outputs = []
     for(let selector in DOMSelectors) {
         const input = getDOMObject(selector)
